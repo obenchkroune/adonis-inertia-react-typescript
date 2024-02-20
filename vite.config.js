7e8792a59b2c812afa2612d3190af5a8f27d13e4
obenchkr <obenchkr@student.1337.ma>
2024-02-20 22:15:18 +0100

    fix import alias

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
 import adonisjs from '@adonisjs/vite/client'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url'
 
 export default defineConfig({
-  // resolve: {
-  //   alias: {
-  //     "@css/*": p
-  //   }
-  // }
+  resolve: {
+    alias: [
+      {
+        find: 'ts',
+        replacement: fileURLToPath(new URL('./resources/ts'), import.meta.url),
+      },
+      {
+        find: 'css',
+        replacement: fileURLToPath(new URL('./resources/css'), import.meta.url),
+      },
+    ],
+  },
   plugins: [
     adonisjs({
       entrypoints: ['resources/ts/app.tsx', 'resources/css/app.css'],
